4f924e5ed2a2c887f935d53fb38230651dfcd7af
Vitor Cezila <vcezila@gmail.com>
2021-01-31 11:46:41 -0300

    Making changes to fragments to make the app lighter

--- HPQuiz/app/src/main/java/com/example/hpquiz/MainActivity.kt
+++ HPQuiz/app/src/main/java/com/example/hpquiz/MainActivity.kt
@@ -3,18 +3,15 @@ package com.example.hpquiz
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.hpquiz.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        ma_startB.setOnClickListener{
-            val intent2 = Intent(this, CategoryActivity::class.java)
-            startActivity(intent2)
-            finish()
-        }
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-# harry-potter-quiz
-
+# HarryPotterQuiz
  My first application made just to introduce me to android studio
-
-# Screenshots
-
-![](screenshots/screen1.png)
